777ad54336d00566d75608eeb93f125417e95189
Karim Mamdouh <karimmamdouh178@gmail.com>
2026-05-15 10:29:19 +0300

    feat: sample tracking workflow

--- server/index.js
+++ server/index.js
@@ -350,6 +350,7 @@ app.use("/analytics", require("./routes/analytics"));
 app.use("/whatsapp", require("./routes/whatsapp.routes"));
 app.use("/reconciliation", require("./routes/reconciliation"));
 app.use("/auth", require("./routes/auth"));
+app.use("/tracked-samples", require("./routes/tracked_samples"));
 
 // Global error handler
 app.use((error, req, res, next) => {
